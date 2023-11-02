6e425a63c1ac2b9003e6cd04c1fe27ba3f8e6b84
cookedbee <kaycha4682@gmail.com>
2023-11-02 13:12:04 -0300

    gui for new user

---
+++ src/project:kbc/GUI newUser.java
@@ -0,0 +1,15 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class userInfo extends JFrame implements ItemListener, ActionListener{
+	static JFrame f;
+	static JLabel 1, 11, 13;
+	static JComboBox c1, c2;
+	
+	public static void main(String[] args){
+		
+	}
+}
+
+	
