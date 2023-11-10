5127d81bb7b2b0ca33be2976acc141100e848ffd
julign <h3a7p@unb.ca>
2023-11-10 15:37:52 -0400

    Added Book, BookInfo and RatingsReviews

---
+++ Book.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Book {
+
+	String title;
+	String author;
+	String genre;
+	ArrayList<RatingsReviews> ratingsReviews;
+	
+	public Book(String titleIn, String authorIn, String genreIn) {
+		title = titleIn;
+		author = authorIn;
+		genre = genreIn;
+		ratingsReviews = new ArrayList<RatingsReviews>();
+	}
+	
+	void addRating(int ratingIn, String ratedByIn) {
+		RatingsReviews ratingReview = new RatingsReviews(ratingIn, ratedByIn);
+		ratingsReviews.add(ratingReview);
+	}
+	
+	void addReview(int ratingIn, String reviewIn, String ratedByIn) {
+		RatingsReviews ratingReview = new RatingsReviews(ratingIn, reviewIn, ratedByIn);
+		ratingsReviews.add(ratingReview);
+	}
+	
+	double avgRating() {
+		if(ratingsReviews.isEmpty()) {
+			return 0.0;
+		}
+		int totalRating = 0;
+		for(RatingsReviews ratingReview : ratingsReviews) {
+			totalRating += ratingReview.getRating();
+		}
+		return (double) totalRating/ratingsReviews.size();
+	}
+
+}
---
+++ RatingsReviews.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class RatingsReviews {
+	
+	int rating;
+	String review;
+	String ratedBy;
+	
+	public RatingsReviews(int ratingIn, String reviewIn, String ratedByIn) {
+		rating = ratingIn;
+		review = reviewIn;
+		ratedBy = ratedByIn;  
+	}
+	
+	public RatingsReviews(int ratingIn, String ratedByIn) {
+		rating = ratingIn;
+		ratedBy = ratedByIn;  
+	}
+	
+	int getRating() {
+		return rating;
+	}
+	
+	String getReview(){
+		return review;
+	}
+	
+	String getRatedBy() {
+		return ratedBy;
+	}
+}
