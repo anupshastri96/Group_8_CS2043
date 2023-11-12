bdc2a8385e6124d56b0ed5f7b9b466f84bf18faf
S-M-Flynn <sarah.flynn@unb.ca>
2023-11-11 22:14:03 -0400

    Update start screen

--- src/project/views/StartScreen.java
+++ src/project/views/StartScreen.java
@@ -1,23 +1,14 @@
 package project.views;
 
 import project.controller.UserLoginController;
-
 import javafx.application.Application;
-
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-
 
 public class StartScreen extends Application{
-	
-	Scene scene0;
-	
+		
 	@Override
 	public void start(final Stage primaryStage){
 				
-		primaryStage.setTitle("Starting programe");
-		primaryStage.setScene(scene0);
-		primaryStage.show();
 		UserLoginController.userSelectView(primaryStage);
 	}
 	
