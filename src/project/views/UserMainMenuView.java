68569adc7480c4f948fb400d063949cee9b47946
cookedbee <kaycha4682@gmail.com>
2023-11-13 20:53:26 -0400

    Update UserMainMenuView.java

--- src/project/views/UserMainMenuView.java
+++ src/project/views/UserMainMenuView.java
@@ -2,7 +2,7 @@
 
 package project.views;
 
-import project.controller.newUserController; //im assuming it will be called that
+import project.controller.UserMainMenuController; //im assuming it will be called that
 import javafx.application.Application;
 import javafx.scene.control.ComboBox;
 import javafx.collections.ObservableList;
@@ -17,4 +17,28 @@ import javafx.scene.control.Button;
 import javafx.geometry.Pos;
 import javafx.event.ActionEvent;
 
-public class UserMainMenuView
+public class UserMainMenuView{
+    static Scene scene4;
+
+    public static void userMainMenuView(Stage stage){
+
+        Button checkoutButton = new Button("Check Out a Book");
+        checkoutButton.setOnAction(e -> CheckoutController.checkout(???)); // new controller
+		Button ratingButton = newButton("Rate and Review a Book");
+		ratingButton.setOnAction(e -> RatingController.rate(???));// new controller
+		Button currentCheckoutButton = new Button("Books Currently Checked Out");
+		currentCheckoutButton.setOnAction(e -> CurrentScheckoutController.currentlyCheckedOut(???));// new controller
+		Button reservedButton = new Button("Books Reserved");
+		reservedButton.setOnAction(e -> ReservedController.currentlyReserved(???));// new controller
+		Button recomendedButton = new Button("Recomended Books");
+		recomendedButton.setOnAction(e -> RecomendedController.recomended(???));// new controller
+		Button editButton = new Button("Edit Profile");
+		c
+		VBox layout4 = new VBox(20);
+		layout4.setAlignment(Pos.CENTER);
+		layout4.getChildren().addAll(checkoutButton, ratingButton, currentCheckoutButton, reservedButton, recomendedButton, editButton);
+		scene4 = new Scene(layout4, 450, 700);
+
+        stage.setScene(scene4);
+        stage.show();
+    }
