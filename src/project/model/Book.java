019a3bbbb7a756c96e6f04c0bd3c1e1ccc1c7f83
julign <146350306+julign@users.noreply.github.com>
2023-11-16 18:41:23 -0400

    Added sorting methods in Book.java

--- src/project/model/Book.java
+++ src/project/model/Book.java
@@ -35,4 +35,50 @@ public class Book implements Comparable<Book>{
 		}
 		return (double) totalRating/ratingsReviews.size();
 	}
-}
+
+	//Sort automatically by title alphabetically
+	@Override
+	public int compareTo(Book other){
+		return this.title.compareTo(other.title);
+	}
+
+	//Sorting by Author
+	public static class AuthorComparator implements Comparator<Book>{
+		@Override
+		public int compare(Book a, Book b){
+			if(a.author != null && b.author != null){
+				return a.author.compareTo(b.author);
+			}
+			return a.title.compareTo(b.title);
+		}
+	}
+
+	//Sorting by genre
+	public static class GenreComparator implements Comparator<Book>{
+		@Override
+		public int compare(Book a, Book b){
+			if(a.genre != null && b.genre != null){
+				if(a.genre.equals(b.genre)){
+					return a.title.compareTo(b.title);
+				}
+				return a.genre.compareTo(b.genre);
+			}
+			return a.title.compareTo(book2.title);
+		}
+	}
+
+	//Sorting by availabilty
+	public static AvailabilityComparator implements Comparator<Book>{
+		@Override
+		public int compare(Book a, Book b){
+			int i = Boolean.compare(a.isAvailable(), b.isAvailable()); //sort by availability
+			if(i != 0){
+				return i;
+			}
+			int j = a.author.compareTo(b.author); //if same sort by author
+			if(j != 0){
+				return j;
+			}
+			return a.title.compareTo(b.title); //if same sort by title
+		}
+	}
