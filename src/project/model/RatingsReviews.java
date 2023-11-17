b4915af5c91e62b122843d4f3dc9eaa7eff2ad1f
S-M-Flynn <sarah.flynn@unb.ca>
2023-11-17 12:51:09 -0400

    import statements added

--- src/project/model/RatingsReviews.java
+++ src/project/model/RatingsReviews.java
@@ -1,6 +1,8 @@
 package project.model;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.list;
 
 public class RatingsReviews {
