d5d5faeae89c9edeb9f94a2b64570c5ac51032cc
julign <146350306+julign@users.noreply.github.com>
2023-11-16 23:29:19 -0400

    Updated BookInfo.java

--- src/project/model/BookInfo.java
+++ src/project/model/BookInfo.java
@@ -1,5 +1,6 @@
 package project.model;
-public class BookInfo {
+import java.util.*;
+public class BookInfo implements Comparable<BookInfo>{
 
 	String bookID;
 	String publicationDate;
@@ -52,4 +53,44 @@ public class BookInfo {
 		return userName;
 	}
 
+	//sorts based on dueDate
+	@Override
+	public int compareTo(BookInfo other){
+		if(this.dueDate == null && other.dueDate == null){
+			return 0;
+		}else if (this.dueDate == null){
+			return 1;
+		}else if (other.dueDate == null{
+			return -1;
+		}
+		return this.dueDate.compareTo(other.dueDate);
+	}
+	//sort by user and then by dueDate if a user has multiple books checked out
+	public static class userBookComparator implements Comparator<BookInfo>{
+		@Override
+		public int compare(BookInfo a, BookInfo b){
+			if(a.userName == null && b.userName == null){
+				return 0;
+			}else if(a.userName == null){
+				return -1;
+			}else if(b.userName == null){
+				return -1;
+			}
+
+			int i = a.userName.compareTo(b.userName);
+			if(i != 0){
+				return i;
+			}
+
+			if(a.dueDate == null && b.dueDate == null){
+				return 0;
+			}else if(a.dueDate == null){
+				return 1;
+			}else if(b.dueDate == null){
+				return -1;
+			}
+			return a.dueDate.compareTo(b.dueDate);
+		}
+	}
+}
 }
