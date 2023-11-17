b538dd5a2f6560a94bcb6956f11f0e4c8647d7a8
julign <146350306+julign@users.noreply.github.com>
2023-11-17 00:26:48 -0400

    added recommendation system

--- src/project/model/RatingsReviews.java
+++ src/project/model/RatingsReviews.java
@@ -1,10 +1,13 @@
 package project.model;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.list;
+
 public class RatingsReviews {
 	
-	int rating;
-	String review;
-	String ratedBy;
+	private int rating;
+	private String review;
+	private String ratedBy;
 	
 	public RatingsReviews(int ratingIn, String reviewIn, String ratedByIn) {
 		rating = ratingIn;
@@ -18,15 +21,50 @@ public class RatingsReviews {
 		review = "";
 	}
 	
-	int getRating() {
+	public int getRating() {
 		return rating;
 	}
 	
-	String getReview(){
+	public String getReview(){
 		return review;
 	}
 	
-	String getRatedBy() {
+	public String getRatedBy() {
 		return ratedBy;
 	}
+
+	public void getTopRecommended(List<Book> bookList){
+		List<UserReviews> reviewedBooks = new ArrayList<>();
+		for(Book book: bookList){
+			for(RatingsReviews ratingReview : book.getRatingsReviews()){
+				if(ratedBy.equals(ratingReview.getRatedBy())){
+					reviewedBooks.add(new UserReviewed(book, ratingReview.getRating()));
+				}
+			}
+		}
+		Collections.sort(reviewedBooks);
+		printRecommendations(ratedBy, reviewedBooks);
+	}
+
+	//r stands for recommendation
+	private void printRecommendations(String userNameIn, List<Book.UserReviewedBook> rIn){//r stands for recommendation
+		System.out.println("Hi " + userNameIn + ", here are some recommendations for you!");
+		int count = 0;
+		for(Book.UserReviewedBook r : rIn){
+			if(count < 5){
+				Book current = r.getBook();
+				System.out.println("	Title: " + r.getTitle());
+				System.out.println("	Author: " + r.getAuthor());
+				System.out.println("	Genre: " + r.getGenre());
+				System.out.println("	Rating: " + r.getRating());
+				System.out.println("-------------------------------");
+				count++;
+			}else{
+				break;
+			}
+		}
+	}
 }
+
+
+
