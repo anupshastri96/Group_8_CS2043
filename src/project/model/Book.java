1a56295a4eb2884de6314673cb11b893842a202a
julign <146350306+julign@users.noreply.github.com>
2023-11-17 09:58:57 -0400

    Update Book.java

--- src/project/model/Book.java
+++ src/project/model/Book.java
@@ -1,5 +1,6 @@
 package project.model;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Book implements Comparable<Book>{
 
@@ -7,12 +8,14 @@ public class Book implements Comparable<Book>{
 	String author;
 	String genre;
 	ArrayList<RatingsReviews> ratingsReviews;
+	BookInfo relatedInfo;
 	
-	public Book(String titleIn, String authorIn, String genreIn) {
+	public Book(String titleIn, String authorIn, String genreIn, String bookIdIn, String publicationDateIn) {
 		title = titleIn;
 		author = authorIn;
 		genre = genreIn;
 		ratingsReviews = new ArrayList<RatingsReviews>();
+		relatedInfo = new BookInfo(bookIdIn, publicationDateIn);
 	}
 	
 	public void addRating(int ratingIn, String ratedByIn) {
@@ -45,6 +48,26 @@ public class Book implements Comparable<Book>{
 		return null;
 	}
 
+	public String getTitle(){
+		return title;
+	}
+
+	public String getAuthor(){
+		return author;
+	}
+
+	public String getGenre(){
+		return genre;
+	}
+
+	public BookInfo getRelatedInfo(){
+		return relatedInfo;
+	}
+
+	public ArrayList<RatingsReviews> getRatingReviews(){
+		return ratingsReviews;
+	}
+
 	//Sort automatically by title alphabetically
 	@Override
 	public int compareTo(Book other){
@@ -72,15 +95,15 @@ public class Book implements Comparable<Book>{
 				}
 				return a.genre.compareTo(b.genre);
 			}
-			return a.title.compareTo(book2.title);
+			return a.title.compareTo(b.title);
 		}
 	}
 
 	//Sorting by availabilty
-	public static AvailabilityComparator implements Comparator<Book>{
+	public static class AvailabilityComparator implements Comparator<Book>{
 		@Override
 		public int compare(Book a, Book b){
-			int i = Boolean.compare(a.isAvailable(), b.isAvailable()); //sort by availability
+			int i = Boolean.compare(a.relatedInfo.getIsAvailable(), b.relatedInfo.getIsAvailable()); //sort by availability
 			if(i != 0){
 				return i;
 			}
@@ -98,7 +121,7 @@ public class Book implements Comparable<Book>{
 
 	public void printBookRating(){
 		System.out.println("Ratings and Reviews for " + title + ":");
-		for(RatingsReviews ratingReview : ratingsReview){
+		for(RatingsReviews ratingReview : ratingsReviews){
 			System.out.println("	Rating: " + ratingReview.getRating());
 			System.out.println("	Review: " + ratingReview.getReview());
 			System.out.println("	Rated By: " + ratingReview.getRatedBy());
